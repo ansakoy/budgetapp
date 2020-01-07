737e66cd609e2bff4bcd3f7ea73ae458bb894adb
ansakoy <ansakoy@gmail.com>
2020-01-07 22:15:45 +0300

    added models to script

--- budget_app.py
+++ budget_app.py
@@ -1,12 +1,52 @@
 import datetime
 
 from flask import Flask, render_template, url_for, flash, redirect
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 from forms import RegistrationForm, LoginForm
 
 app = Flask(__name__)
-
 app.config['SECRET_KEY'] = '3c4a39427e7e103f6fa0043631e06003'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+
+# Models - temporary
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(30), unique=True, nullable=False)
+    email = db.Column(db.String(150), unique=True, nullable=False)
+    image_file = db.Column(db.String(20), nullable=False, default='default.jpg')
+    password = db.Column(db.String(60), nullable=False)
+    expenses = db.relationship('Expense', backref='user', lazy=True)
+
+    def __repr__(self):
+        return f"User('{self.username}', '{self.email}', '{self.image_file}')"
+
+
+class Expense(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False, default=datetime.datetime.now)
+    comment = db.Column(db.Text)
+    price = db.Column(db.Float, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey('category.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+
+    def __repr__(self):
+        return f"Expense('{self.date}', '{self.category}', '{self.price}')"
+
+
+class Category(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    category_name = db.Column(db.String(100), unique=True, nullable=False)
+    expenses = db.relationship('Expense', backref='category', lazy=True)
+
+    def __repr__(self):
+        return f"Category('{self.category_name}')"
+
 
 EXPENSES = [
     {
