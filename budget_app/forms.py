f4d7c21d29246193758fab9f3df4a0b9e6bfe7a4
ansakoy <ansakoy@gmail.com>
2020-01-09 03:12:23 +0300

    user_profile

--- budget_app/forms.py
+++ budget_app/forms.py
@@ -1,8 +1,10 @@
 from flask_wtf import FlaskForm
+from flask_login import current_user
 from wtforms import StringField, PasswordField, SubmitField, BooleanField
 from wtforms.validators import DataRequired, Length, Email, EqualTo, ValidationError
 
 from budget_app.models import User
+
 
 class RegistrationForm(FlaskForm):
     username = StringField(
@@ -43,3 +45,25 @@ class LoginForm(FlaskForm):
     )
     remember = BooleanField('Remember Me')
     submit = SubmitField('Sign In')
+
+
+class UpdateAccountForm(FlaskForm):
+    username = StringField(
+        'Username',
+        validators=[DataRequired(), Length(min=2, max=30)]
+    )
+    email = StringField(
+        'Email',
+        validators=[DataRequired(), Email()]
+    )
+    submit = SubmitField('Update')
+
+    def validate_username(self, username):
+        if username.data != current_user.username:
+            if User.query.filter_by(username=username.data).first():  # None if not found
+                raise ValidationError('This username already exists')
+
+    def validate_email(self, email):
+        if email.data != current_user.email:
+            if User.query.filter_by(email=email.data).first():  # None if not found
+                raise ValidationError('User with this email already exists')
